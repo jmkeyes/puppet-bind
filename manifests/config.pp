679eaa99f9b8aa14ea25f583c5fbd13833da8841
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2014-10-23 16:24:39 -0700

    Move parameters from ::bind::config to ::bind.

--- manifests/config.pp
+++ manifests/config.pp
@@ -31,50 +31,6 @@ class bind::config (
   validate_absolute_path($::bind::config::rndc_key_link)
   validate_absolute_path($::bind::config::bind_keys_file)
 
-  validate_string($::bind::dnssec_enable)
-  validate_re($::bind::dnssec_enable, '^(yes|no)$', "\$dnssec_enable must be one of 'yes' or 'no'!")
-
-  validate_string($::bind::dnssec_validation)
-  validate_re($::bind::dnssec_validation, '^(yes|no|auto)$', "\$dnssec_validation must be one of 'yes', 'no' or 'auto'!")
-
-  validate_string($::bind::dnssec_lookaside)
-  validate_re($::bind::dnssec_lookaside, '^(yes|no|auto)$', "\$dnssec_lookaside must be one of 'yes', 'no' or 'auto'!")
-
-  validate_bool($::bind::purge_configuration)
-
-  validate_string($::bind::check_names_master)
-  validate_re($::bind::check_names_master, '^(none|warn|fail|ignore)$', "\$check_names_master must be one of 'none', 'warn', 'fail', or 'ignore'!")
-
-  validate_string($::bind::check_names_slave)
-  validate_re($::bind::check_names_slave, '^(none|warn|fail|ignore)$', "\$check_names_slave must be one of 'none', 'warn', 'fail', or 'ignore'!")
-
-  validate_string($::bind::check_names_response)
-  validate_re($::bind::check_names_response, '^(none|warn|fail|ignore)$', "\$check_names_response must be one of 'none', 'warn', 'fail', or 'ignore'!")
-
-  validate_string($::bind::version)
-
-  validate_string($::bind::use_notify)
-  validate_re($::bind::use_notify, '^(yes|no)$', "\$use_notify must be one of 'yes' or 'no'!")
-
-  validate_string($::bind::use_recursion)
-  validate_re($::bind::use_recursion, '^(yes|no)$', "\$use_recursion must be one of 'yes' or 'no'!")
-
-  validate_bool($::bind::use_default_zones)
-  validate_bool($::bind::use_rfc1918_zones)
-
-  validate_array($::bind::listen_ipv4)
-  validate_array($::bind::listen_ipv6)
-
-  validate_array($::bind::allow_update)
-  validate_array($::bind::allow_update_forwarding)
-  validate_array($::bind::allow_transfer)
-  validate_array($::bind::allow_notify)
-  validate_array($::bind::allow_recursion)
-  validate_array($::bind::allow_query)
-
-  validate_string($::bind::forward_policy)
-  validate_re($::bind::forward_policy, '^(first|only)$', "\$forward_policy must be one of 'first' or 'only'!")
-  validate_array($::bind::forwarders)
 
   file { $::bind::config::config_directory:
     ensure  => directory,
