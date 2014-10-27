33d82d1c60acb0e320f3414648192a12f93a80e0
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2014-10-27 13:00:05 -0700

    Add skeleton for logging channels and categories.

---
+++ manifests/resource/logging_category.pp
@@ -0,0 +1,6 @@
+# == Define: bind::resource::logging_category
+
+define bind::resource::logging_category {
+  fail('bind::resource::logging_category resources are not implemented yet.')
+}
+
---
+++ manifests/resource/logging_channel.pp
@@ -0,0 +1,6 @@
+# == Define: bind::resource::logging_channel
+
+define bind::resource::logging_channel {
+  fail('bind::resource::logging_channel resources are not implemented yet.')
+}
+
